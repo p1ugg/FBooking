e9594245727a2aa4d19f26ecd6dc8ab4febdd778
p1ugg <67374872+p1ugg@users.noreply.github.com>
2023-04-01 01:29:39 +0300

    Add files via upload

--- time_docs.py
+++ time_docs.py
@@ -1,12 +1,14 @@
 import csv
 from datetime import datetime, date, timedelta
 from pprint import pprint
+
 dict_docs = dict()
 with open('data/doc_list.csv', 'r', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     for row in spamreader:
         b = row
         dates = b[2].split(',')
+        time_break = dates[-1]
         now = datetime.now()
         sp = list()
         for i in range(0, 8):
@@ -25,6 +27,17 @@ with open('data/doc_list.csv', 'r', newline='') as csvfile:
                 sp_time = list()
                 for i in range(start, endd):
                     sp_time.append(f'{i}:00-{i + 1}:00')
+
+                tb = list(map(int, str(time_break.split()[1]).replace('00', '').replace(':', '').split('-')))
+                start = tb[0]
+                endd = tb[1]
+                sp_tb = list()
+                for i in range(start, endd):
+                    sp_tb.append(f'{i}:00-{i + 1}:00')
+                for i in sp_tb:
+                    if i in sp_time:
+                        sp_time.remove(i)
+
             new_sp.append(sp_time)
 
         Dates = dict()
@@ -35,9 +48,3 @@ with open('data/doc_list.csv', 'r', newline='') as csvfile:
             Dates[i] = new_sp[dd]
 
         dict_docs[row[0]] = Dates
-# pprint(dict_docs)
-for i in dict_docs.items():
-    n = i[1]
-    # print(i[0])
-    # for j in n.items():
-        # print(f'{j[0]} - {j[1]}')
