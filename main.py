1297e569c35b7081ef19ce19362cdfbf1a8a24c4
Smubzik <67224186+Smubzik@users.noreply.github.com>
2023-04-01 16:20:23 +0300

    Первые кастомизационные правки и изменения

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from aiogram.dispatcher.filters import Text
 import csv
 from time_docs import dict_docs
 
+# Инециализация бота
 TOKEN_API = token
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
 kb_start = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_start.add('Запись')
-kb_start.add('Расписание')
-kb_start.add('Наши специалисты')
-kb_start.add('Врач. Учётная запись')
+kb_start.add('🔔 Запись 🛎')
+kb_start.add('🕑 Расписание 🕘')
+kb_start.add('☑ Наши специалисты ✅')
+kb_start.add('🩺 Врач. Учётная запись 🌡')
 
 kb_docs = ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -26,21 +27,37 @@ with open('data/doc_list.csv', 'r', newline='') as csvfile:
         kb_docs.add(row[0])
         docs_sp.append(row)
 
+greetings = f'''😇 Приветствуем! 😇 \n\nВы находитесь в <u>telegram-боте</u> клиники <code>ООО"Бобёр"</code> 🦫\n
+❗️Наша клиника оснащена <b>первоклассным оборудованием</b>, 
+ведь счастливая жизнь пациента - это наша забота 🩻\n\n❕️Удобная инфраструктура \
+здания клиники гарантирует вам <i>быстрое и понятное</i> перемещение по ней\n
+‼️Лучшие специалисты страны с большим стажем работы\n
+🩹 Вы можете ознакомиться \
+с отзывами клиники на данном {'<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">сайте</a>'}\n\n
+🔅 Данный бот даёт возможность записаться на приём к врачу, \
+а также доступ к некоторому сопутсвующему функционалу. Для\
+ продолжения работы выберите одну(1) из четырёх(4) функций в меню кнопок 🔅'''
+
+
 
 # print(docs_sp)
 
-@dp.message_handler(commands=['start'])
+# 🩺🩻🌡🩹❗️❕🔅〽️🌀🕑▫️🔸🔻🔺🟢🔵⚪️🟣🔹☑️🟩🔔🕘📢‼️🛎🧬🗓📆
+
+@dp.message_handler(commands=['start'])  # Обработчик команды /start
 async def start(message: types.Message):
-    await message.answer(
-        text=f'Приветствую! Данный бот поможет вам легко и быстро, а самое главное удобно записаться к врачу',
-        reply_markup=kb_start)
+    beaver_center = open('data/beavercenter.jpg', 'rb')
+    await message.delete()
+    await bot.send_photo(message.chat.id, beaver_center, caption=greetings,reply_markup=kb_start, parse_mode='HTML')
+    # await bot.send_message(chat_id=message.from_user.id, text=greetings,
+    #                        reply_markup=kb_start, disable_web_page_preview=True, parse_mode='HTML')
 
 
-@dp.message_handler(Text(equals='Запись'))
+@dp.message_handler(Text(equals='🔔 Запись 🛎'))  # Обработчик на нажатие кнопки "Запись"
 async def booking(message: types.Message):
     await message.answer(
-        text=f'Выберите врача ниже',
-        reply_markup=kb_docs)
+        text=f'🧬 Выберите <b>врача</b> <u>из</u> ниже представленного <u>списка</u>: 🧬',
+        reply_markup=kb_docs, parse_mode="HTML")
 
     specalist_booking = message.text
     now = datetime.now()
@@ -53,13 +70,13 @@ async def booking(message: types.Message):
     @dp.message_handler()
     async def datee(message: types.Message):
         await message.answer(
-            text=f'Выберите дату ниже',
-            reply_markup=kb_date)
+            text=f'🗓 Выберите <i>удобную</i> для вас дату <u>из</u> <b>ниже представленного</b> <u>списка</u>:',
+            reply_markup=kb_date, parse_mode="HTML")
 
         date_booking = message.text
 
 
-@dp.message_handler(Text(equals='Расписание'))
+@dp.message_handler(Text(equals='🕑 Расписание 🕘'))
 async def schedule(message: types.Message):
     await message.answer(
         text=f'Выберите специалиста ниже',
@@ -79,7 +96,7 @@ async def schedule(message: types.Message):
             reply_markup=kb_docs)
 
 
-@dp.message_handler(Text(equals='Наши специалисты'))
+@dp.message_handler(Text(equals='☑ Наши специалисты ✅'))
 async def specialists(message: types.Message):
     await message.answer(
         text=f'Выберите специалиста ниже',
@@ -98,7 +115,7 @@ async def specialists(message: types.Message):
                                      reply_markup=kb_docs)
 
 
-@dp.message_handler(Text(equals='Врач. Учётная запись'))
+@dp.message_handler(Text(equals='🩺 Врач. Учётная запись 🌡'))
 async def account(message: types.Message):
     await message.answer(
         text=f'work',
